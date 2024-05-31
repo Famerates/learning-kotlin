4eb04670b35c20f26492a1510c5b254be4f1baf0
Famerates <koskinenjimi@gmail.com>
2024-05-31 15:50:04 +0300

    corrected exitProcess

--- src/main/kotlin/com/famerates/goog/peruna.kt
+++ src/main/kotlin/com/famerates/goog/peruna.kt
@@ -8,5 +8,5 @@ fun perunantti() {
     println("Number of balls: ${c1.balls}")
     println("Name of the game: ${c1.amogus}")
     println("Are you going to ${c1.sugondese}?")
-    return exitProcess(status = 0)
+    exitProcess(status = 0)
 }
