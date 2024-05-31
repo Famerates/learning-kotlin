d3539547ecc2af0ee6112b22bef43b8d4b51f465
Famerates <koskinenjimi@gmail.com>
2024-05-31 15:47:16 +0300

    added class Ligma.kt

---
+++ src/main/kotlin/com/famerates/goog/Ligma.kt
@@ -0,0 +1,7 @@
+package com.famerates.goog
+
+class Ligma {
+    var balls = 2
+    var amogus = "AMOGUS"
+    var sugondese = "SUGONDESE"
+}
--- src/main/kotlin/com/famerates/goog/peruna.kt
+++ src/main/kotlin/com/famerates/goog/peruna.kt
@@ -1,5 +1,12 @@
 package com.famerates.goog
 
+import kotlin.system.exitProcess
+
 fun perunantti() {
-    println("PERUNANTTI")
+    val c1 = Ligma()
+
+    println("Number of balls: ${c1.balls}")
+    println("Name of the game: ${c1.amogus}")
+    println("Are you going to ${c1.sugondese}?")
+    return exitProcess(status = 0)
 }
