97de6c9b6e1b639e61a3c4cdafbdbfcb10067132
Famerates <koskinenjimi@gmail.com>
2024-09-07 14:06:04 +0300

    replace if-else with when

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,23 +6,25 @@ fun main() {
     println("Which feature would you like to use? [1] Useless things, [2] Pinging, [3] elasticity calculation, [4] would you rather, [5] skyblock or not?")
     val inputA = readln()
     val inputC = inputA.toIntOrNull()
-    if (inputC == 1) {
-        useless()
-    }
-    else if (inputC == 2) {
-        pinging()
-    }
-    else if (inputC == 3) {
-        elasticityValues()
-    }
-    else if (inputC == 4) {
-        unlimitedBacon()
-    }
-    else if (inputC == 5) {
-        skyblockOrNot()
-    }
-    else {
-        println("unknown option: $inputA")
+    when (inputC) {
+        1 -> {
+            useless()
+        }
+        2 -> {
+            pinging()
+        }
+        3 -> {
+            elasticityValues()
+        }
+        4 -> {
+            unlimitedBacon()
+        }
+        5 -> {
+            skyblockOrNot()
+        }
+        else -> {
+            println("unknown option: $inputA")
+        }
     }
     exitProcess(status = 0)
 }
