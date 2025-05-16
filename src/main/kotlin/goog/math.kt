b401129d59a0e5e2f2954ed0e6a18a89ade0f290
Famerates <108594235+famerates@users.noreply.github.com>
2025-05-16 18:23:29 +0300

    add timer to math quiz

--- src/main/kotlin/goog/math.kt
+++ src/main/kotlin/goog/math.kt
@@ -1,10 +1,10 @@
 package com.famerates.goog
 import kotlin.random.Random
+import kotlin.system.measureTimeMillis
 
 fun mathQuiz() {
-    // TODO timer
     println("This is a 5 question math quiz")
-
+    var totalTime = 0L
     repeat(5) {
         val num1 = Random.nextInt(1,10)
         val num2 = Random.nextInt(1,10)
@@ -25,13 +25,18 @@ fun mathQuiz() {
                 num3 = num1 - num2
             }
         }
-        print("$num1 $sym $num2 = ")
-        val input = readln().toIntOrNull()
-        if (input == num3) {
-            println("Correct")
-        } else {
-            println("Incorrect")
+        val timeTaken = measureTimeMillis {
+            print("$num1 $sym $num2 = ")
+            val input = readln().toIntOrNull()
+            if (input == num3) {
+                println("Correct")
+            } else {
+                println("Incorrect")
+            }
         }
+        totalTime = totalTime + timeTaken
+        println("Time taken to answer: ${timeTaken / 1000.0} seconds")
     }
+    println("Total time taken: ${totalTime / 1000.0} seconds")
 return
 }
